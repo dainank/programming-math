7d1477230dda5583196a25925c1d87febd5b984d
dainank <dainank@proton.me>
2022-10-12 11:50:12 +0200

    more junk

--- linear-transformations/drawings.py
+++ linear-transformations/drawings.py
@@ -6,6 +6,33 @@ from vectors import *
 vectors_414 = [(x, y) for x in range(0,6) for y in range(0,6)]
 Sv = [(x**2, y**2) for x in range(0,6) for y in range(0,6)]
 
+vectors_416 = [
+    (3, 2), (0, 0), (3, -2), (0, 0)
+]
+
+vectors_416b = [
+    (3, -2), (0, 0), 
+]
+draw(
+    Points(*vectors_416, color=blue),
+    Arrow(*[(3, 2), (0, 0)], color=black),
+    Arrow(*[(3, -2), (0, 0)], color=black),
+    Segment(*[(3, -2), (3, 2)], color=purple),
+    grid=(1, 1),
+    nice_aspect_ratio=False,
+    window_title='Exercise 4.16'
+)
+
+draw(
+    Points(*(vectors_416 * 2), color=blue),
+    Arrow(*[(6, 4), (0, 0)], color=black),
+    Arrow(*[(6, -4), (0, 0)], color=black),
+    Segment(*[(6, 4), (6, -4)], color=purple),
+    grid=(1, 1),
+    nice_aspect_ratio=False,
+    window_title='Exercise 4.16'
+)
+
 draw(
     Points(*vectors_414),
     grid=(1, 1),
@@ -14,8 +41,15 @@ draw(
 )
 
 draw(
-    Points(*Sv),
+    Points(*vectors_414),
     grid=(1, 1),
     nice_aspect_ratio=False,
     window_title='Exercise 4.14'
 )
+
+def linear_combination(scalars, *vectors):
+   scaled = [scale(s, v) for s, v in zip(scalars, vectors)]
+   return add(*scaled)
+
+def transform_standard_basis(transform):
+   return transform((1,0,0)), transform((0,1,0)), transform((0,0,1))
